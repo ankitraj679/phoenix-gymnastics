bd2b12457755f3f5a8c21a702331385d97b1092a
ankitraj679 <ankitrajpramar789@gmail.com>
2025-02-04 17:56:59 +0530

    Second major commit
    04/02/2025

--- js/main.js
+++ js/main.js
@@ -228,29 +228,6 @@ AOS.init({
   });
   
 
-  // navigation
-  // var OnePageNav = function() {
-  // 	$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
-  // 	 	e.preventDefault();
-
-  // 	 	var hash = this.hash,
-  // 	 			navToggler = $('.navbar-toggler');
-  // 	 	$('html, body').animate({
-  // 	    scrollTop: $(hash).offset().top
-  // 	  }, 700, 'easeInOutExpo', function(){
-  // 	    window.location.hash = hash;
-  // 	  });
-
-  // 	  if ( navToggler.is(':visible') ) {
-  // 	  	navToggler.click();
-  // 	  }
-  // 	});
-  // 	$('body').on('activate.bs.scrollspy', function () {
-  // 	  console.log('nice');
-  // 	})
-  // };
-  // OnePageNav();
-
   // magnific popup
   $(".image-popup").magnificPopup({
     type: "image",
